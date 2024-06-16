scarfacered/Paper

Add datapack registration lifecycle event

test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -1,14 +1,33 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.datapack.DatapackRegistrar;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
+
+import static net.kyori.adventure.text.Component.text;
 
 public class TestPluginBootstrap implements PluginBootstrap {
 
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         // io.papermc.testplugin.brigtests.Registration.registerViaBootstrap(context);
+        final LifecycleEventManager<BootstrapContext> manager = context.getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.DATAPACK_REGISTRATION, event -> {
+            final DatapackRegistrar registrar = event.registrar();
+            try {
+                final URI uri = Objects.requireNonNull(TestPluginBootstrap.class.getResource("/pack")).toURI();
+                registrar.addDatapack(uri, "test", text("HEY THERE"));
+            } catch (final URISyntaxException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
 }

secret9999999/Server2000

test-plugin/src/main/java/io/papermc/testplugin/TestPluginBootstrap.java
@@ -1,14 +1,33 @@
 package io.papermc.testplugin;
 
+import io.papermc.paper.datapack.DatapackRegistrar;
 import io.papermc.paper.plugin.bootstrap.BootstrapContext;
 import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 import org.jetbrains.annotations.NotNull;
+
+import static net.kyori.adventure.text.Component.text;
 
 public class TestPluginBootstrap implements PluginBootstrap {
 
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         // io.papermc.testplugin.brigtests.Registration.registerViaBootstrap(context);
+        final LifecycleEventManager<BootstrapContext> manager = context.getLifecycleManager();
+        manager.registerEventHandler(LifecycleEvents.DATAPACK_DISCOVERY, event -> {
+            final DatapackRegistrar registrar = event.registrar();
+            try {
+                final URI uri = Objects.requireNonNull(TestPluginBootstrap.class.getResource("/pack")).toURI();
+                registrar.discoverPack(uri, "test");
+            } catch (final URISyntaxException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
 }